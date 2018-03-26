1696fa9812e98f7eddd65df784741ee5c319e804
oshi192 <yuriykoss@gmail.com>
2018-03-26 11:03:14 +0300

    dev: fixed YuriyFindBug

--- Lecture 5 (SpaceX)/RocketLauncher/src/PlanetDistances.java
+++ Lecture 5 (SpaceX)/RocketLauncher/src/PlanetDistances.java
@@ -1,14 +1,18 @@
 public enum PlanetDistances {
-    MOON(0.0025),VENUS(0.27),MERCURY(0.6),MARS(0.52),SUN(1),JUPITER(4.18),SATURN(8.51),URANUS(18.14),NEPTUNE(29),PLUTO(39),CENTEROFGALAXY(1638000000);
+    MOON("MOON",0.0025),VENUS("VENUS",0.27),MERCURY("MERCURY",0.6),MARS("MARS",0.52),
+    SUN("SUN",1),JUPITER("JUPITER",4.18),SATURN("SATURN",8.51),URANUS("URANUS",18.14),
+    NEPTUNE("NEPTUNE",29),PLUTO("PLUTO",39),CENTEROFGALAXY("CENTEROFGALAXY",1638000000);
     public double distance;
-    PlanetDistances(double distance)
+    public String name;
+    PlanetDistances(String name,double distance)
     {
+        this.name=name;
         this.distance=distance;
     }
 
     public static boolean asMyEnum(String str) {
         for (PlanetDistances d : PlanetDistances.values()) {
-            if (d.name().equalsIgnoreCase(str))
+            if (d.name.equals(str))
                 return true;
         }
         return false;
