a3de721f83dc09b1a87e197687199a7c5b5648bf
oshi192 <yuriykoss@gmail.com>
2018-03-31 08:57:16 +0300

    dev 2 first commit

---
+++ Game/src/main/java/Race.java
@@ -0,0 +1,12 @@
+public class Race {
+    protected boolean isPreferance;
+    protected float kDamage;
+    protected float health;
+    protected int countActions;
+    protected int [] actions;
+    Race(){
+
+    }
+
+
+}
