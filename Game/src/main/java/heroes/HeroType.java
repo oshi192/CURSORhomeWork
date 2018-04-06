4a69782c7202e046a3b0c18621cbe5db6b693d85
oshi192 <yuriykoss@gmail.com>
2018-04-06 11:27:18 +0300

    dev2 refactor9

--- Game/src/main/java/heroes/HeroType.java
+++ Game/src/main/java/heroes/HeroType.java
@@ -5,15 +5,15 @@ import lombok.Getter;
 @Getter
 public enum HeroType {
 
-    C11("Human", "Paladin", 10, 0, 10, 13),
-    C12("Human", "Archer", 11, 0, 5, 15),
-    C13("Human", "Mag", 15, 15, 2, 1),
-    C21("Elves", "Warrior", 9, 0, 12, 15),
-    C22("Elves", "Super archer", 13, 0, 7, 3),
-    C23("Elves", "Druid", 15, 15, 2, 3),
-    C31("Demons", "Barbarian", 15, 0, 5, 10),
-    C32("Demons", "Hells archer", 14, 0, 2, 1),
-    C33("Demons", "Necromancer", 15, 15, 2, 2);
+    C11("Human", "Paladin", 10, 10, 13),
+    C12("Human", "Archer", 11, 5, 15),
+    C13("Human", "Mag", 15,  2, 1),
+    C21("Elves", "Warrior", 9,  12, 15),
+    C22("Elves", "Super archer", 13, 7, 3),
+    C23("Elves", "Druid", 15,  2, 3),
+    C31("Demons", "Barbarian", 15, 5, 10),
+    C32("Demons", "Hells archer", 14, 2, 1),
+    C33("Demons", "Necromancer", 15,  2, 2);
 
     private String type;
     private String race;
@@ -22,13 +22,13 @@ public enum HeroType {
     private int armor;
     private int critical;
 
-    HeroType(String race, String type, int damage, int healling, int critical, int armor) {
+    HeroType(String race, String type, int damage,  int critical, int armor) {
         this.type = type;
         this.armor = armor;
         this.damage = damage;
-        this.healing = healling;
         this.race = race;
         this.critical = critical;
+        this.healing=(damage==15)?15:0;
     }
 
     public void getInfo() {
